912effa227e83b039dbe46d42692352a36b325fd
Jonathan Morfin <jmorfin776@gmail.com>
2019-09-10 15:03:35 -0400

    heroku ready

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,16 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
 
+
+
+#flask
 app = Flask(__name__)
+
+
+
+#bootstrap
+bootstrap = Bootstrap(app)
+
+
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -3,29 +3,5 @@ from flask import render_template, url_for, redirect, flash
 
 
 @app.route('/')
-@app.route('/index')
 def index():
-    return render_template('index.html', title='Home')
-
-@app.route('/about-me')
-def about_me():
-    return render_template('about-me.html', title='About Me')
-
-
-@app.route('/photos')
-def photos():
-    return render_template('photos.html', title='Photos')
-
-
-@app.route('/follow Me')
-def follow_me():
-    return render_template('follow-me.html', title='Follow Me')
-
-
-@app.route('/contact')
-def contact():
-    return render_template('contact.html', title='Contact')
-
-@app.route('/blog')
-def blog():
-    return render_template('blog.html', title='Blog')
+    return ''
